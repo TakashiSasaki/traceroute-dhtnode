e8a0b15b082d7fc90b3abf4fa54a46fd6ba8f780
Takashi SASAKI <sasaki@stretch>
2018-12-19 08:45:55 +0000

    printing to sys.stderr

--- traceroute_v6.py
+++ traceroute_v6.py
@@ -24,10 +24,10 @@ class UdpSocket:
         self.buffer = bytes(512)
 
     def send(self):
-        print("Sending UDP to ", self.dest, " with TTL = ", self.ttl, end="")
+        print("Sending UDP to ", self.dest, " with TTL = ", self.ttl, end="", flush=True, file=sys.stderr)
         self.socket.sendto(self.buffer, (self.dest, self.port))
         self.sentTime = time.time()
-        print(" done")
+        print(" done", flush=True, file=sys.stderr)
 
     def close(self):
         self.socket.close()
@@ -39,23 +39,23 @@ class IcmpSocket:
         self.socket = socket.socket(IcmpSocket.ADDRESS_FAMILY, socket.SOCK_RAW, IcmpSocket.PROTOCOL_NUMBER_ICMP)
 
     def receive(self):
-        print("Receiving ICMP .. ", end="", flush=True)
+        print("Receiving ICMP .. ", end="", flush=True, file=sys.stderr)
         self.receivedBytes, self.receivedAddress = self.socket.recvfrom(512)
         self.receivedTime = time.time()
         self.type = int(self.receivedBytes[20])
         self.code = int(self.receivedBytes[21])
         self.data = self.receivedBytes[24:]
         print(len(self.receivedBytes),"bytes received from ", self.receivedAddress[0], 
-                ", ICMP type = ", self.type, ", ICMP code = ", self.code)
-        print("ICMP payload = ", bytes.hex(self.data))
+                ", ICMP type = ", self.type, ", ICMP code = ", self.code, flush=True, file=sys.stderr)
+        print("ICMP payload = ", bytes.hex(self.data), flush=True, file=sys.stderr)
         if self.type == 3 and self.code == 1:
-            print("ICMP : Host unreachable")
+            print("ICMP : Host unreachable", flush=True, file=sys.stderr)
         elif self.type == 3 and self.code == 3:
-            print("ICMP : Port unreachable")
+            print("ICMP : Port unreachable", flush=True, file=sys.stderr)
         elif self.type == 11 and self.code == 0:
-            print("ICMP : TTL equals 0 during transit")
+            print("ICMP : TTL equals 0 during transit", flush=True, file=sys.stderr)
         else:
-            print("ICMP : other reason")
+            print("ICMP : other reason", flush=True, file=sys.stderr)
 
     def close(self):
         self.socket.close()
@@ -76,15 +76,15 @@ def traceroute(dest):
 
         try:
             icmpSocket.receive()
-            print("RTT = ", (icmpSocket.receivedTime - udpSocket.sentTime) * 1000, " ms")
+            print("RTT = ", (icmpSocket.receivedTime - udpSocket.sentTime) * 1000, " ms", flush=True, file=sys.stderr)
 
             try:
-                print("FQDN = ", socket.gethostbyaddr(icmpSocket.receivedAddress[0]))
+                print("FQDN = ", socket.gethostbyaddr(icmpSocket.receivedAddress[0]), flush=True, file=sys.stderr)
             except socket.error:
                 pass
 
         except socket.error:
-            print (" timeout")
+            print (" timeout", flush=True, file=sys.stderr)
 
         finally:
             udpSocket.close()
@@ -93,7 +93,7 @@ def traceroute(dest):
         ttl += 1
 
         if icmpSocket.receivedAddress[0] == dest or ttl > MAX_TTL:
-            print("reached final destination.")
+            print("reached final destination.", flush=True, file=sys.stderr)
             break
 
 if __name__ == "__main__":
