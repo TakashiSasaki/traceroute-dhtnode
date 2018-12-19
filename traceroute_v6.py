9047d7da2d7018a89473b933f6000ebe76f56380
Takashi SASAKI <sasaki@stretch>
2018-12-19 05:11:41 +0000

    enrich messages

--- traceroute_v6.py
+++ traceroute_v6.py
@@ -1,4 +1,5 @@
-#bibliography
+#!/usr/bin/python3 
+# Bibliography
 # https://qiita.com/gandarla/items/df2c5ac3f9f9485e494a
 # http://d.hatena.ne.jp/addition/20130501/1367403754
 # https://gist.github.com/jcjones/0f3f11a785a833e0a216
@@ -7,35 +8,34 @@ import sys
 import socket
 import time
 
+ADDRESS_FAMILY = socket.AF_INET6 # for IPv6
+ADDRESS_FAMILY = socket.AF_INET # for IPv4
+PROTOCOL_NUMBER_ICMP = socket.getprotobyname("ipv6-icmp") # for IPv4
+PROTOCOL_NUMBER_ICMP = socket.getprotobyname("icmp") # for IPv4
+PROTOCOL_NUMBER_UDP = socket.getprotobyname("udp") 
+
 def traceroute(dest_name, port, max_hops):
     #IPv6はgetaddrinfoでIPアドレスを取得する
     #dest_addr = socket.getaddrinfo(dest_name, None)
     dest_addr = dest_name
 
 
-    print("target ==> %s (%s)" % (dest_name, dest_addr))
-
-    socket.setdefaulttimeout(10)
-
-    icmp = socket.getprotobyname('ipv6-icmp')
-
-    udp = socket.getprotobyname('udp')
+    socket.setdefaulttimeout(15)
 
     ttl = 1
 
-    totalRTT = 0
-
     while True:
+        recv_socket = socket.socket(ADDRESS_FAMILY, socket.SOCK_RAW, PROTOCOL_NUMBER_ICMP)
 
-        recv_socket = socket.socket(socket.AF_INET6, socket.SOCK_RAW, icmp)
-
-        send_socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM, udp)
+        send_socket = socket.socket(ADDRESS_FAMILY, socket.SOCK_DGRAM, PROTOCOL_NUMBER_UDP)
 
         send_socket.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
 
         recv_socket.bind(("", port))
 
+        print("Sending UDP to ", dest_addr, " with TTL = ", ttl, end="")
         send_socket.sendto(bytes(512), (dest_addr, port))
+        print(" done")
 
         startTime = time.time()
 
@@ -44,45 +44,49 @@ def traceroute(dest_name, port, max_hops):
 
 
         try:
-            curr_name, curr_addr = recv_socket.recvfrom(512)
-
+            #curr_name, curr_addr = recv_socket.recvfrom(512)
+            print("Receiving ICMP .. ", end="", flush=True)
+            received_bytes, received_address = recv_socket.recvfrom(512)
             endTime = time.time()
 
-            curr_addr = curr_addr[0]
+            icmp_type = int(received_bytes[20])
+            icmp_code = int(received_bytes[21])
+            icmp_data = received_bytes[24:]
+            print(len(received_bytes),"bytes received from ", received_address[0], 
+                    ", ICMP type = ", icmp_type, ", ICMP code = ", icmp_code)
+            print("ICMP payload = ", bytes.hex(icmp_data))
+            print("RTT = ", (endTime - startTime) * 1000, " ms")
+
+            if icmp_type == 3 and icmp_code == 1:
+                print("ICMP : Host unreachable")
+            elif icmp_type == 3 and icmp_code == 3:
+                print("ICMP : Port unreachable")
+            elif icmp_type == 11 and icmp_code == 0:
+                print("ICMP : TTL equals 0 during transit")
+            else:
+                print("ICMP : other reason")
 
             try:
-                curr_name = socket.gethostbyaddr(curr_addr)[0]
-
+                print("FQDN = ", socket.gethostbyaddr(received_address[0]))
             except socket.error:
-                curr_name = curr_addr
+                pass
 
         except socket.error:
-            pass
+            print (" timeout")
 
         finally:
             send_socket.close()
             recv_socket.close()
 
-            RTT = (endTime - startTime) * 1000
-            totalRTT += RTT
-
-
-
-        if curr_addr is not None:
-            curr_host = "%s (%s)" % (curr_name, curr_addr)
-
-        else:
-            curr_host = "*"
-
-
-        print("%d  %s" % (ttl, curr_host))
-        print("   %f" % RTT + " ms")
-
         ttl += 1
 
         if curr_name == dest_name or curr_addr == dest_addr or ttl > max_hops:
+            print("reached final destination.")
             break
 
 if __name__ == "__main__":
-    traceroute(str(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
+    if len(sys.argv) == 4:
+        traceroute(str(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
+    else:
+        traceroute("133.71.200.55", 1000, 20)
 
